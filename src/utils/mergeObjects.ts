c2b84091637c87cc53b94c748d560962257b718d
Lucas <jlucasmelob558@gmail.com>
2026-05-23 20:46:23 -0300

    refactor: update mergeObjects signature to use generics

--- src/utils/mergeObjects.ts
+++ src/utils/mergeObjects.ts
@@ -1,15 +1,13 @@
 import { isPlainObject } from './isPlainObject';
 
-export function mergeObjects<T>(
-    objA: Record<string, any>,
-    objB: Record<string, any>
-): T {
+export function mergeObjects<T = Record<string, any>>(objA: Partial<T>, objB: Partial<T>): T {
     const result: Record<string, any> = { ...objA };
 
-    for (const [ key, valueB ] of Object.entries(objB)) {
+    for (const [key, valueB] of Object.entries(objB)) {
         const valueA = result[key];
 
-        if (isPlainObject(valueA) && isPlainObject(valueB)) result[key] = mergeObjects(valueA, valueB);
+        if (isPlainObject(valueA) && isPlainObject(valueB))
+            result[key] = mergeObjects(valueA, valueB);
         else result[key] = valueB;
     }
 
