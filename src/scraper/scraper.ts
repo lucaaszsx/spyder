434c75c61b9f8db42b23e2384620bbd2ecc65991
Lucas <jlucasmelob558@gmail.com>
2026-05-23 20:48:23 -0300

    refactor: update Scraper to use the new HttpClient

--- src/scraper/scraper.ts
+++ src/scraper/scraper.ts
@@ -1,26 +1,17 @@
-import { defaultOptions, mergeObjects } from '../utils';
-import { EventEmitter } from 'node:events';
+/*import { mergeObjects } from '../utils';
+import { defaultOptions } from '../constants';
 import { ScraperOptions } from '../types';
-import { Events } from '../constants';
 import { HttpClient } from '../http';
 
-export class Scraper extends EventEmitter {
-    private httpClient: HttpClient;
-    private debug: boolean;
+export class Scraper {
+    protected readonly httpClient: HttpClient;
+    readonly debug: boolean;
 
-    constructor(options: ScraperOptions = defaultOptions) {
-        super();
+    constructor(options: Partial<ScraperOptions> = {}) {
+        const resolved = mergeObjects<ScraperOptions>(defaultOptions, options);
 
-        options = mergeObjects<ScraperOptions>(defaultOptions, options);
-
-        this.httpClient = new HttpClient(options.httpClient);
-        this.debug = !!options.debug;
-
-        if (this.debug)
-            this.on(Events.Debug, function(message) {
-                // todo: log message
-            });
+        this.httpClient = new HttpClient(resolved.httpClient);
+        this.debug = resolved.debug;
     }
-
-
 }
+*/
