e845e79f4de7f72ee6ee2856152b7c8f0330c55a
Lucas <jlucasmelob558@gmail.com>
2026-06-04 18:53:32 -0300

    feat: add barrel file at src/schema

---
+++ src/schema/index.ts
@@ -0,0 +1,8 @@
+export * from './schema';
+export * from './errors';
+export * from './payload';
+
+export * from './primitives/string';
+export * from './primitives/number';
+export * from './primitives/boolean';
+export * from './primitives/literal';
