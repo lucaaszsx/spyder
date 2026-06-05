19cbbef34af843550776843e9f6f1a375362b241
Lucas <jlucasmelob558@gmail.com>
2026-06-05 10:54:12 -0300

    refactor: use Set instead of an array on literal schema

--- src/schema/primitives/literal.ts
+++ src/schema/primitives/literal.ts
@@ -3,26 +3,25 @@ import type { SpyderSchemaPayload } from '../payload';
 import * as util from '../../utils';
 
 export class SpyderLiteralSchema extends SpyderPrimitiveSchemaBase<util.Literal> {
-    public values: util.LiteralArray = [];
+    public values: Set<util.Literal>;
 
     constructor(...values: util.LiteralArray) {
         if (values.length === 0)
             throw new Error('Cannot create a literal schema without specifying any literal values');
 
         super(false, null);
-
-        if (values != null) this.values = [values].flat();
+        this.values = new Set([values].flat());
     }
 
     protected _parse(
         _def: SpyderPrimitiveSchemaDef,
         payload: SpyderSchemaPayload<util.Literal>
     ): void {
-        if (!this.values.includes(payload.value))
+        if (!this.values.has(payload.value))
             payload.issue({
                 code: 'invalid_value',
-                message: `Invalid value provided, expected one of: ${this.values.map(util.parsePrimitive).join(' | ')}`,
-                expected: this.values,
+                message: `Invalid value provided, expected one of: ${[...this.values].map(util.parsePrimitive).join(' | ')}`,
+                expected: [...this.values],
                 input: payload.value
             });
     }
