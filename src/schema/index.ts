214871305c1e486d0f71ce5f3887be1690c92afc
Lucas <jlucasmelob558@gmail.com>
2026-06-05 17:03:34 -0300

    fix: fix primitives schemas barrel file re-exporting old schema base definitions

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -2,8 +2,10 @@ export * from './schema';
 export * from './errors';
 export * from './payload';
 
-export * from './primitives/base';
 export * from './primitives/string';
 export * from './primitives/number';
 export * from './primitives/boolean';
 export * from './primitives/literal';
+
+export * from './compound/base';
+export * from './compound/array';
