2ef0911a504df8bfefcf9c0781741297b38988e3
Lucas <jlucasmelob558@gmail.com>
2026-06-05 15:02:29 -0300

    refactor: re-export base primitive declarations

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -2,6 +2,7 @@ export * from './schema';
 export * from './errors';
 export * from './payload';
 
+export * from './primitives/base';
 export * from './primitives/string';
 export * from './primitives/number';
 export * from './primitives/boolean';
