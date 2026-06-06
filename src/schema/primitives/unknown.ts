98b06def5aa9edec9a5fb7b28935aad2fe217936
Lucas <jlucasmelob558@gmail.com>
2026-06-05 22:10:58 -0300

    docs: fix comment on unknown schema

--- src/schema/primitives/unknown.ts
+++ src/schema/primitives/unknown.ts
@@ -3,7 +3,7 @@ import type { SpyderSchemaPayload } from '../payload';
 
 export class SpyderUnknownSchema extends SpyderSchemaBase<unknown> {
     protected _parse(_def: SpyderSchemaDef, _payload: SpyderSchemaPayload): void {
-        // Does not return nothing, "unknown" schema allows any value
+        // Don't do anything, "unknown" schema allows any value
         return;
     }
 }
