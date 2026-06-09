b1a22ab9ae9e0b6d828e5951deedd5e0081290fc
Lucas <jlucasmelob558@gmail.com>
2026-06-09 00:23:15 -0300

    fix(schema): fix typo in SpyderUnknownSchema

--- src/schema/primitives/unknown.ts
+++ src/schema/primitives/unknown.ts
@@ -2,7 +2,7 @@ import type { SpyderSchemaContext } from '../context';
 import { SpyderSchemaBase } from '../schema';
 
 export class SpyderUnknownSchema extends SpyderSchemaBase<unknown> {
-    protected _parse(_payload: SpyderSchemaContext, value: unknown): unknown {
+    protected _parse(_ctx: SpyderSchemaContext, value: unknown): unknown {
         // Don't do anything, "unknown" schema allows any value
         return value;
     }
